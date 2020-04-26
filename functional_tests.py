218c9335cf5f4cb53d8c76d012c7caee374e04da
CaiBindi <1091110955@qq.com>
2020-04-26 21:54:40 +0800

    Model for list Items and associated migration -2

--- functional_tests.py
+++ functional_tests.py
@@ -17,46 +17,53 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertIn(row_text,[row.text for row in rows])
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
-		#Edith heard about a cool new online to-do app. She goes 
+		# Edith heard about a cool new online to-do app. She goes 
 		# to check its homepage,
 		self.browser.get('http://localhost:8000')
 		
-		#She notices the page title and header mention to-do lists
-		#assert 'To-Do' in browser.title, "Browser title was" + browser.title #(4)
+		# She notices the page title and header mention to-do lists
+		# assert 'To-Do' in browser.title, "Browser title was" + browser.title #(4)
 		self.assertIn('To-Do',self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do',header_text)
 		
 		
-		#She is invited to enter a to-do item straight away
-		
-		#She types "Buy peacock feathers" into a text box
-		#(Edith's hobby is tying fly-fishing lures)
+		# She is invited to enter a to-do item straight away
 		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
+
+		# She types "Buy peacock feathers" into a text box
+		# (Edith's hobby is tying fly-fishing lures)
 		inputbox.send_keys('Buy peacock feathers')
-		#When she hits enter, the page updates,and now the page lists
-		#"1:Buy peacock feathers" as an item in a to-do list
+
+		# When she hits enter, the page updates,and now the page lists
+		# "1:Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
 		self.check_for_row_in_list_table('1:Buy peacock feathers')
 
-		#There is still a text box inviting her to add another item. She 
-		#enters "Use peacock feathers to make a fly" (Edith is very methodical)
+		# There is still a text box inviting her to add another item. She 
+		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
-		#The page updates again, and now shows both items on her list
+
+		# The page updates again, and now shows both items on her list
 		self.check_for_row_in_list_table('1:Buy peacock feathers')
 		self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
 
-		#Edith wonders whether the site will remember her list. Then she goes
-		#that the site has generated a unique URL for her -- there is some
-		#explanatory text to that effect
-		self.fail('Finish the test!')
-		#She visits that URL - her to-do list is still there
+		# Edith wonders whether the site will remember her list. Then she goes
+		# that the site has generated a unique URL for her -- there is some
+		# explanatory text to that effect
 		
-		#Satisfied, she goes back to sleep
+		# She visits that URL - her to-do list is still there
+		
+		# Satisfied, she goes back to sleep
+		self.fail('Finish the test!')
 		
 		
 if __name__ == '__main__':
